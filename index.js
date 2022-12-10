472aff4213206cb15133b0cfe7cb2b42d6d410ec
Kennysaruni <kenny.saruni@student.moringaschool.com>
2022-12-10 15:45:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  // Get the strings or numbers from the user.
+  // Take a temporary variable that holds the numbers. 
+  // Reverse the given number.
+  // Compare the original number with the reversed number
+  // If the temporary and original number are same, it the number or string is a Palindrome.
+  // Else the given string or number is not the Palindrome.
+    const len = word.length
+    for(let i=0; i<len/2;i++){
+      if (word[i] !== word[len - 1 - i]){
+        return false
+      }
+    }
+    return true
+}  
 
 /* 
   Add your pseudocode here
