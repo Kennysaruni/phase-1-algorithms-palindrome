fb5938ac18ea9bd8c0ce66ab15a1740dd15112cd
Kennysaruni <kenny.saruni@student.moringaschool.com>
2022-12-10 15:46:35 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -18,10 +18,19 @@ function isPalindrome(word) {
 /* 
   Add your pseudocode here
 */
+//initialise a variable that holds the string length
+//use the string length on the for loop to iterate to half the words length
+//in the for loop include an if to check whether corresponding characters in the two halves are equal
+// if they are return true
+//if not return false
 
 /*
   Add written explanation of your solution here
 */
+//The length of the string is calculated using the length built in function
+//the for loop iterates to half of the string 
+//the if checks whether the correspinding last and forst characters are the same 
+// if they arent equal it is considered a palindrome
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
